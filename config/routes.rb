7527742ecbd9d5301934b4269912437681930847
Ailin Chen <ailinchenirene@gmail.com>
2023-05-30 21:00:27 +0200

    Rails Wikinimous

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,11 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  get '/articles', to: 'articles#index'
+  post 'articles', to: 'articles#create'
+  get '/new', to: 'articles#new', as: :new_article
+  get '/article/:id/edit', to: 'articles#edit', as: :edit_article
+  delete 'article/:id', to: 'articles#destroy'
+  get '/article/:id', to: 'articles#show', as: :article
+  patch '/article/:id', to: 'articles#update'
 end
